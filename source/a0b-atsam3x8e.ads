057120ec59acacbe68b9dae8b04c7ca091229e7d
Vadim Godunko <vgodunko@gmail.com>
2024-04-22 09:07:02 +0400

    Initial version.

---
+++ source/a0b-atsam3x8e.ads
@@ -0,0 +1,25 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+package A0B.ATSAM3X8E
+  with Pure
+is
+
+   --  Peripheral identifiers/external interrupt identifiers.
+
+   Timer_Counter_Channel_0 : constant := 27;
+   Timer_Counter_Channel_1 : constant := 28;
+   Timer_Counter_Channel_2 : constant := 29;
+   Timer_Counter_Channel_3 : constant := 30;
+   Timer_Counter_Channel_4 : constant := 31;
+   Timer_Counter_Channel_5 : constant := 32;
+   Timer_Counter_Channel_6 : constant := 33;
+   Timer_Counter_Channel_7 : constant := 34;
+   Timer_Counter_Channel_8 : constant := 35;
+
+end A0B.ATSAM3X8E;
