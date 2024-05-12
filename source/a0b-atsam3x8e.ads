5c9088df666a6b95b2a968def25be5020cf5503d
Vadim Godunko <vgodunko@gmail.com>
2024-05-12 12:29:32 +0400

    Peripheral identifiers of USART controllers.

--- source/a0b-atsam3x8e.ads
+++ source/a0b-atsam3x8e.ads
@@ -15,21 +15,26 @@ is
 
    --  Peripheral identifiers/external interrupt identifiers.
 
-   Parallel_IO_Controller_A      : constant := 11;
-   Parallel_IO_Controller_B      : constant := 12;
-   Parallel_IO_Controller_C      : constant := 13;
-   Parallel_IO_Controller_D      : constant := 14;
+   Parallel_IO_Controller_A                                  : constant := 11;
+   Parallel_IO_Controller_B                                  : constant := 12;
+   Parallel_IO_Controller_C                                  : constant := 13;
+   Parallel_IO_Controller_D                                  : constant := 14;
 
-   Serial_Peripheral_Interface_0 : constant := 24;
+   Universal_Synchronous_Asynchronous_Receiver_Transmitter_0 : constant := 17;
+   Universal_Synchronous_Asynchronous_Receiver_Transmitter_1 : constant := 18;
+   Universal_Synchronous_Asynchronous_Receiver_Transmitter_2 : constant := 19;
+   Universal_Synchronous_Asynchronous_Receiver_Transmitter_3 : constant := 20;
 
-   Timer_Counter_Channel_0       : constant := 27;
-   Timer_Counter_Channel_1       : constant := 28;
-   Timer_Counter_Channel_2       : constant := 29;
-   Timer_Counter_Channel_3       : constant := 30;
-   Timer_Counter_Channel_4       : constant := 31;
-   Timer_Counter_Channel_5       : constant := 32;
-   Timer_Counter_Channel_6       : constant := 33;
-   Timer_Counter_Channel_7       : constant := 34;
-   Timer_Counter_Channel_8       : constant := 35;
+   Serial_Peripheral_Interface_0                             : constant := 24;
+
+   Timer_Counter_Channel_0                                   : constant := 27;
+   Timer_Counter_Channel_1                                   : constant := 28;
+   Timer_Counter_Channel_2                                   : constant := 29;
+   Timer_Counter_Channel_3                                   : constant := 30;
+   Timer_Counter_Channel_4                                   : constant := 31;
+   Timer_Counter_Channel_5                                   : constant := 32;
+   Timer_Counter_Channel_6                                   : constant := 33;
+   Timer_Counter_Channel_7                                   : constant := 34;
+   Timer_Counter_Channel_8                                   : constant := 35;
 
 end A0B.ATSAM3X8E;
